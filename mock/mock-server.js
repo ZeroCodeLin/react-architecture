292f575619aee6694568113343da015cd7adca5a
lemon <540846207@qq.com>
2018-06-08 17:41:05 +0800

    添加mock数据模拟

---
+++ mock/mock-server.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+
+app.use(bodyParser())
+
+
+const hello = require('./hello')
+
+app.use(router.routes())
+router.get('/users/list', async(ctx, next) => {
+  ctx.body = hello
+  await next()
+})
+
+// error-handling
+app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx)
+});
+
+app.listen(3000);
+console.log('app started at port 3000')
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,35 @@
 import React, {PureComponent} from 'react'
+import { Route, Switch, Redirect, Link } from 'react-router-dom'
+
+import Page1 from './Page1.jsx'
+import Page2 from './Page2.jsx'
+import Page3 from './Page3.jsx'
 
 export default class Home extends PureComponent{
 
     render(){
         return (
-            <div>Hello World!</div>
+            <div style={styles.container} >
+                <ul>
+                    <li><Link to="/">首页</Link></li>
+                    <li><Link to="/">首页</Link></li>
+                    <li><Link to="/">首页</Link></li>
+                </ul>
+                <Switch>
+                    <Route  path="/page1" component={Page1}/>
+                    <Route  path="/page2" component={Page2}/>
+                    <Route  path="/page3" component={Page3}/>
+
+                    <Redirect exact from="/" to='/page1' />
+                </Switch>
+            </div>
         )
+    }
+}
+
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center'
     }
 }
