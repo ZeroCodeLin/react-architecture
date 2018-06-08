d6663dd3de44f84ca35ed9835267d15ed0690c31
lemon <540846207@qq.com>
2018-06-08 18:05:29 +0800

    update page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react'
+import React, { PureComponent } from 'react'
 import { Route, Switch, Redirect, Link } from 'react-router-dom'
 
 import Page1 from './Page1.jsx'
@@ -9,19 +9,22 @@ export default class Home extends PureComponent{
 
     render(){
         return (
-            <div style={styles.container} >
-                <ul>
-                    <li><Link to="/">首页</Link></li>
-                    <li><Link to="/">首页</Link></li>
-                    <li><Link to="/">首页</Link></li>
-                </ul>
-                <Switch>
-                    <Route  path="/page1" component={Page1}/>
-                    <Route  path="/page2" component={Page2}/>
-                    <Route  path="/page3" component={Page3}/>
+            <div>
+                <div style={styles.container}>
+                    <Link to="/page1">Page1</Link>
+                    <Link to="/page2">Page2</Link>
+                    <Link to="/page3">Page3</Link>
+                </div>
+                <div style={styles.container}>
+                    <Switch>
+                        <Route  path="/page1" component={Page1}/>
+                        <Route  path="/page2" component={Page2}/>
+                        <Route  path="/page3" component={Page3}/>
 
-                    <Redirect exact from="/" to='/page1' />
-                </Switch>
+                        <Redirect exact from="/" to='/page1' />
+                    </Switch>
+                </div>
+                
             </div>
         )
     }
