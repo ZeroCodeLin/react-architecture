1194e4ffc484e26a995303cf17ce87c7265b396f
lemon <540846207@qq.com>
2018-06-08 17:40:25 +0800

    add page

---
+++ src/routes/Page1.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react'
+// import request from '../utils/request'
+
+export default class Page1 extends PureComponent{
+    
+    render(){
+        return (
+            <div>Hello Page1!</div>
+        )
+    }
+}
---
+++ src/routes/Page3.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react'
+// import request from '../utils/request'
+
+export default class Page3 extends PureComponent{
+    
+    render(){
+        return (
+            <div>Hello Page3!</div>
+        )
+    }
+}
