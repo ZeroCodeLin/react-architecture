d5730ec9664a95ccbbfa09747a9937cb9373caaa
lemon <540846207@qq.com>
2018-06-12 22:35:29 +0800

    抽取开发和生产环境webpack相同配置

---
+++ webpack.base.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractCss = new ExtractTextPlugin("style.css")
+const htmlTemplate = new HtmlWebpackPlugin({template:'./src/index.html',favicon: './public/favicon.png'})
+const config = {
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.[hash:4].js'
+    },
+    module:{
+        rules:[
+            {  
+                test: /\.(woff|eot|ttf|svg|png|jpg)$/,  
+                use: [  
+                    {  
+                        loader: 'url-loader',  
+                        options: {  
+                            limit: '1024' ,
+                            name: '[name].[hash:4].[ext]'  
+                        }                        
+                    },  
+                ]  
+            },
+            {  
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                use: [  
+                    {  
+                        loader: 'url-loader',  
+                        options: {  
+                            limit: '1024',
+                            name: '[name].[hash:4].[ext]'  
+                        }  
+                    },  
+                ]  
+            },
+            {
+                test: /\.(css|less)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ["css-loader","less-loader"]
+                })
+            },
+            {
+                test:/\.(js|jsx)$/,
+                use:"babel-loader",
+                exclude:/node_modules/
+            }
+        ]
+    },
+}
+
+module.exports = {
+    htmlTemplate,
+    extractCss,
+    config
+}
